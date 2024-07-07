79fd2046f33ac8aad53d0a57e219d171a3974ccf
sehyeon7 <worldjade07@gmail.com>
2024-07-07 14:47:02 +0900

    kakaopay user authentication logic 주석

--- kakaopay/models.py
+++ kakaopay/models.py
@@ -7,4 +7,4 @@ class KakaoPay(models.Model):
     partner_order_id=models.CharField(max_length=100)
     partner_user_id=models.CharField(max_length=100)
     point=models.IntegerField(default=0)
-    user=models.ForeignKey(User, on_delete=models.CASCADE, related_name='pay_buyer', null=True)
+    #user=models.ForeignKey(User, on_delete=models.CASCADE, related_name='pay_buyer', null=True)
